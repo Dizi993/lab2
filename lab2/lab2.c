e020f05e57397808620569d25b0944bd924a3832
Данила Копицын <supermuper543@mail.ru>
2024-09-19 13:27:48 +0300

    1.9

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -3,16 +3,14 @@
 void main()
 {
 	setlocale(LC_CTYPE, "RUS");
-	int N=12, K=56;
-	printf("Сейчас %d часов %d минут 00 секунд\n",N,K);
-	printf("Идет %d минута суток\n", N * 60 + K);
-	printf("До полуночи осталось %d часов и %d минут\n", 24 - N, 60 - K);
-	printf("С 8.00 прошло %d секунд\n", N * 60 * 60 + K * 60);
-	printf("Текущий час  = %f суток  и текущая минута = %f часа", N / 24., K / 60.);
-	
 
+	int n = 3;
 
-
-
+	printf("Дано:\n");
+	printf("\t%d\n", n);
+	printf("\t%d\n", 333);
+	printf("       ------\n");
+	printf("Oтвет:\n");
+	printf("\t%+08.3f",(n / 333.));	
 }
 
